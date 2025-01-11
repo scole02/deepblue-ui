f51678a5c511b0e084bc3538bbffce7ae2ef5b60
scole02 <samcole107@gmail.com>
2025-01-11 15:14:11 -0800

    Add test media

--- deepblue-django/deepblue_maps/models.py
+++ deepblue-django/deepblue_maps/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.gis.db import models as gis_models
 
 class ModelParams(models.Model):
-    filepath = models.CharField(max_length=255)
+    filepath = models.FileField(max_length=255, upload_to='models/')
     name = models.CharField(max_length=100)
     classes = models.JSONField()  # Stores a list of classesE
 
@@ -15,7 +15,7 @@ class Detection(models.Model):
     likely_class = models.CharField(max_length=50)
     img = models.ImageField(upload_to='detections/')  # Stores the image file
     time = models.DateTimeField(auto_now_add=True)  # Automatically adds the timestamp
-    confidences = models.JSONField()  # Stores a list of confidence scores
+    confidences = models.JSONField()  # Stores a list of confidence scores with class names as keys
 
     def __str__(self):
         return f"Detection: {self.likely_class} at {self.time}"
