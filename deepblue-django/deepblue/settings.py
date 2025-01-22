ff2cbc14a5d056c94a99320ffd951b0b32c7057e
scole02 <samcole107@gmail.com>
2025-01-22 14:34:29 -0800

    Add ngrok in trusted hosts and origins

--- deepblue-django/deepblue/settings.py
+++ deepblue-django/deepblue/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aiupu+_4p^=s4&&u+qunxaeq^szj$g230k=)=9dt@mb)z9*hhr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok-free.app']
 
 
 # Application definition
